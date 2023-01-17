e7113a3a764f707c47864ab6bf4888b49ad501fd
Venkatesh Aleti <venkihema222.aleti@gmail.com>
2023-01-17 15:12:26 +0530

    photo project using matrerial ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import * as React from "react";
+import imagesData from "./images.json";
+import {
+  AppBar,
+  Container,
+  ImageList,
+  ImageListItem,
+  TextField,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6"> Photography </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container style={{ marginTop: "80px", textAlign: "center" }}>
+        <Typography variant="h2">Welcome to my Photography</Typography>
+        <Typography paragraph>One stop destination for your Photos</Typography>
+      </Container>
+      <Container>
+        <ImageList rowHeight={150} cols={4}>
+          {imagesData.map((imageObj) => (
+            <ImageListItem key={imageObj.id}>
+              <img
+                src={imageObj.img}
+                alt={imageObj.name}
+                style={{ height: "150px" }}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Container>
+      <Container>
+        <center>
+          <form>
+            <Typography variant="h4"> ContactUs</Typography>
+            <TextField
+              style={{ width: "50%", margin: "5px" }}
+              type={"text"}
+              label="name"
+            />
+            <br />
+            <TextField
+              style={{ width: "50%", margin: "5px" }}
+              type={"email"}
+              label="Email"
+            />
+            <br />
+            <TextField
+              style={{ width: "50%", margin: "5px" }}
+              type={"number"}
+              label="PhoneNumber"
+            />
+            <br />
+            <TextField
+              style={{ width: "50%", margin: "5px" }}
+              type={"text"}
+              label="Address"
+            />
+            <br />
+            <TextField
+              style={{ width: "50%", margin: "5px" }}
+              type={"text"}
+              label="Purpose"
+            />
+            <br />
+            <Button variant="contained" color="primary">
+              Submit
+            </Button>
+          </form>
+        </center>
+      </Container>
     </div>
   );
 }
